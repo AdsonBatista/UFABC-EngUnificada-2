2d95d9befab0c605ee892e01074cecb78a1aef31
Adson Batista <adson.batista@live.com>
2017-07-25 08:53:59 -0300

    inicio

---
+++ code.c
@@ -0,0 +1,24 @@
+//Bluetooth
+//0 tempo limpo
+//1 tempo sujo
+#define BT_CONN 0
+//
+#define INBOX 1
+#define OUTBOX 5
+
+
+
+// programa principal
+// sensor de toque = 1
+task main()
+{
+ SetSensor(IN_1,SENSOR_TOUCH);
+do
+{
+ statements;
+}
+while (condition);
+
+}
+
+
