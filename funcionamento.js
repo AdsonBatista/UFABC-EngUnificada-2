b5ad5817ed893fe47d76f3295a9e63853b19a313
Adson Batista <adson.batista@live.com>
2017-08-05 18:11:04 -0300

    funcionar.

---
+++ funcionamento.js
@@ -0,0 +1,57 @@
+// Panico
+if(x=0){  
+	// Bluetooth b=0 -> Tempo limpo b=1 -> tempo ruim
+	While(b=0){
+	Funciona 
+		if(tempo anterior de b=1){
+			b=0
+			//inicialização
+			if(y=0){
+				liga motor pá para descer {}
+				liga motor esteira {}
+				liga motor pá para rotacionar {}
+				liga TEMPO 
+				// tempo = contador de tempo
+				// liga caçamba
+				if(TEMPO ligado*){
+					if(distancia > 3/4){
+						funciona
+					} else if (distancia > 2/4){
+						Emite aviso
+					}else if (distancia < 1/4){
+						// para esvaziar
+						if(y=1){
+							desligo motor rotacionamento da pá{}
+							liga motor pá subir {}
+							Tempo de espera {} //lixo da esteira subir
+							desligo motor da esteira {}
+							desliga contador de TEMPO de funcionamento
+						}
+					}
+				}				
+			}
+
+		}else{
+			pleno - Tudo funciona como estava
+			TEMPO Ligado
+		}
+	}else{
+		if(y=1){
+			desligo motor rotacionamento da pá{}
+			liga motor pá subir {}
+			Tempo de espera {}
+			desligo motor da esteira {}
+			desliga contador de tempo de funcionamento
+		}
+
+	}
+}else{
+	if(y=1){
+	desligo motor rotacionamento da pá{}
+	liga motor pá subir {}
+	Tempo de espera {}
+	desligo motor da esteira {}
+	desliga contador de tempo de funcionamento
+	}
+
+}
