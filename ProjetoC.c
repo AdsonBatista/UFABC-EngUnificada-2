aaeaefc5ca4bd3d8e6f8db5c6827c644533f84b5
Adson Batista <adson.batista@live.com>
2017-08-08 11:41:06 -0300

    Update ProjetoC.c

--- ProjetoC.c
+++ ProjetoC.c
@@ -18,56 +18,52 @@
       }
 
       sub desligar() {
-        // paro as pa
-        if (SENSOR_3 > 40) {
-          Coast(OUT_B); //diminuir o motor
+        
+          Coast(OUT_B); //diminuir velocidade do motor da Pã
           // porta, velocidade, angulo, the proportional part P(t), the integrative part I(t) and the derivative part D(t)
           RotateMotorPID(OUT_A, 70, -45, 40, 40, 90);
           // espera 3000ms para fazer outra coisa
           Wait(3000);
-          // paro esteira
+          // Parar motor da esteira
           Off(OUT_C);
         }
-      }
 
       // enquanto panico não pertado ele faz algo
       task main() {
           SetSensor(IN_1, SENSOR_TOUCH); // define sensor entrada 1 como sensor de toque {panico}
           SetSensor(IN_2, SENSOR_TOUCH); // define sensor entrada 2 como sensor de toque {coletor de lixo}
-          //SetSensor(S3,SENSOR_COLORRED,100); // define sensor de cor na entrada 3 utilizando todas as corres {sensor de chuva}
-          //SetSensorColorFull(S3);		// uma apostila seta assim
           SetSensorLight(S3); // uma apostila seta assim
 
           do { // Se sensor 1 não apertado faça...
-            //TextOut (int x, int y, string str,options)
-            TextOut(15, LCD_LINE1, "OK", true);
-            Wait(5000);
-            //colocar rotina aqui
-            do {
+            TextOut(15, LCD_LINE1, "OK", true);   //TextOut (int x, int y, string str,options)
+            Wait(3000); // Espera 3s antes de fazer qualquer coisa
+            
+            do {// Se sensor 2 não apertado faça...
               TextOut(15, LCD_LINE1, "livre", true);
-              Wait(5000);
-              // sensor de cor
-              do { //se ele não esta vendo vermelho
-                TextOut(15, LCD_LINE1, "Sol", true);
-                Wait(5000);
-                ligar();
-              } while (SENSOR_3 > 40); //condição de saida do do
+              Wait(3000);
 
-              if (SENSOR_3 > 40) { // sai do do e l^isso
+              do { // sol/chuva - Se sensor 3...
+                TextOut(15, LCD_LINE1, "Sol", true);
+                Wait(1000);               // Espera 1s
+                ligar();                  // Executa subrotina ligar
+              } while (SENSOR_3 > 40);    // Condição de saida do "do" 
+                  // é necessario esse if? acho que não testar sem.
+              if (SENSOR_3 > 40) { // saio do "do" "sol/chuva" e verifico isso
                 {
                   TextOut(15, LCD_LINE1, "Chuva", true);
-                  Wait(800);
-                  desligar();
+                  Wait(800);              // Espera 1s
+                  desligar();             // Executa subrotina ligar
                 }
               }
-              while (SENSOR_2 != 0);
-              if (SENSOR_2 != 0) {
-                TextOut(15, LCD_LINE1, "Cheio", true);
-                desligar();
-                Wait(5000);
+              while (SENSOR_2 != 0);                  //Saida do do "Livre/Cheio"
+              // é necessario esse if? acho que não testar sem.
+              if (SENSOR_2 != 0) {                    // saio do "do" "livre/Cheio" e verifico isso
+                TextOut(15, LCD_LINE1, "Cheio", true); // Aviso que está cheio
+                desligar();                            // executo a rotina desligar
+                Wait(5000);                            // espero 3s
               }
-            } while (SENSOR_1 == 0)
-            if (SENSOR_1 == 0) {
+            } while (SENSOR_1 == 1)                   // Saida deo sensor Panico
+            if (SENSOR_1 == 1) {                      // Saio do "do" "sensor panico" e verifico 
               //se panico apertodo ele para tudo
               Off(OUT_ABC);
               //TextOut (int x, int y, string str,options)
